f44aed7e4f4627dd4178f8b2dd5fc0dbf0243c34
suhao <suhaoxb@gmail.com>
2023-04-09 00:11:18 +0700

    install and import sqlalchemy

--- database.py
+++ database.py
@@ -1,3 +1,2 @@
 from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, Table
-from sqlalchemy.orm import declarative_base, relationship
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import declarative_base
