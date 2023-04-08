49dae1ea7097569a872979a5e03b9fa085671d01
suhao <suhaoxb@gmail.com>
2023-04-09 00:12:40 +0700

    user table done

--- database.py
+++ database.py
@@ -1,2 +1,17 @@
 from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, Table
 from sqlalchemy.orm import declarative_base
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'users'
+    user_id = Column(Integer, primary_key=True)
+    user_name = Column(String)
+    user_type = Column(String)  # 'professor' or 'student'
+    __mapper_args__ = {
+        'polymorphic_on': user_type
+    }
+
+    def __init__(self, user_id, user_name):
+        self.user_id = user_id
+        self.user_name = user_name
