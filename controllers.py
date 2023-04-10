50aaf44129e68784cb75e8bf3231f78a28b70e14
suhao <suhaoxb@gmail.com>
2023-04-10 17:36:00 +0700

    CRUD StudentController done

--- controllers.py
+++ controllers.py
@@ -8,7 +8,6 @@ class UserController:
         return session.query(User).get(user_id)
 
 
-
 class ProfessorController:
 
     @staticmethod
@@ -34,4 +33,32 @@ class ProfessorController:
         professor = session.query(Professor).get(user_id)
         if professor:
             session.delete(professor)
+            session.commit()
+
+
+class StudentController:
+
+    @staticmethod
+    def create(user_id, user_name):
+        student = Student(user_id=user_id, user_name=user_name)
+        session.add(student)
+        session.commit()
+        return student
+
+    @staticmethod
+    def get(user_id):
+        return session.query(Student).get(user_id)
+
+    @staticmethod
+    def update(user_id, user_name):
+        student = session.query(Student).get(user_id)
+        if student:
+            student.user_name = user_name
+            session.commit()
+
+    @staticmethod
+    def delete(user_id):
+        student = session.query(Student).get(user_id)
+        if student:
+            session.delete(student)
             session.commit()
