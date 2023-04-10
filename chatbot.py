2a2cd780dd62202960aaa7e1308a2a68d77fb663
suhao <suhaoxb@gmail.com>
2023-04-10 17:41:49 +0700

    Set up predefined users and implement button registration function

--- chatbot.py
+++ chatbot.py
@@ -4,6 +4,7 @@ from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
 from telegram.ext import Application, CommandHandler, ContextTypes
 
 from controllers import UserController
+from utils import register
 
 # Enable logging
 logging.basicConfig(
@@ -50,3 +51,37 @@ async def login(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 
         reply_markup = InlineKeyboardMarkup(keyboard)
         await update.message.reply_text("Please choose an option to sign up: ", reply_markup=reply_markup)
+
+
+async def signUpButton(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Parses the CallbackQuery and updates the message text."""
+
+    user_id = update.effective_user.id
+    query = update.callback_query
+    option = query.data
+
+    if option == "professor":
+        if user_id in PROFESSOR_LIST:
+            await query.answer()
+
+            user_id = update.effective_user.id
+            username = update.effective_user.username
+            new_user = register(user_id, username, query.data)
+            context.bot_data["isLogin"] = True
+
+            await query.edit_message_text(f"You are now a {new_user.user_type}. \n" + HELP_MSG)
+        else:
+            await query.edit_message_text(f"You can not sign up as professor, coz you are not on professor list")
+
+    elif option == "student":
+        if user_id not in PROFESSOR_LIST:
+            await query.answer()
+
+            user_id = update.effective_user.id
+            username = update.effective_user.username
+            new_user = register(user_id, username, query.data)
+            context.bot_data["isLogin"] = True
+
+            await query.edit_message_text(f"You are now a {new_user.user_type}. \n" + HELP_MSG)
+        else:
+            await query.edit_message_text(f"You can not sign up as student, coz you are not on professor list")
