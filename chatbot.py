a56295462d14fe914c6b617f01618adaaf5e864b
suhao <suhaoxb@gmail.com>
2023-04-10 17:38:05 +0700

    login function and InlineKeyboardButton created

--- chatbot.py
+++ chatbot.py
@@ -3,6 +3,7 @@ import logging
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
 from telegram.ext import Application, CommandHandler, ContextTypes
 
+from controllers import UserController
 
 # Enable logging
 logging.basicConfig(
@@ -24,3 +25,28 @@ PROFESSOR_LIST = [5138021525]  # Set your predefined user_id here
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text("Welcome! \n\n" + HELP_MSG)
 
+
+async def login(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """
+    first check if user in database.
+    if not:
+        Sends a message with three inline buttons attached.
+    else:
+        Send Welcome message.
+    """
+    user_id = update.effective_user.id
+    # search user by user_id in database:
+    user = UserController.get(user_id)
+    if user:
+        context.bot_data["isLogin"] = True
+        await update.message.reply_text("Welcome Back! " + f" {user.user_name} ({user.user_type}) \n\n")
+    else:
+        keyboard = [
+            [
+                InlineKeyboardButton("professor", callback_data="professor"),
+                InlineKeyboardButton("student", callback_data="student"),
+            ],
+        ]
+
+        reply_markup = InlineKeyboardMarkup(keyboard)
+        await update.message.reply_text("Please choose an option to sign up: ", reply_markup=reply_markup)
